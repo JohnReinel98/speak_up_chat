3ef18f035ce8a9231a2e1b34253c2620c14cc996
John Reinel S. Caluag <johnrenielcaluag1009@gmail.com>
2018-08-19 08:33:34 +0800

    Added Set Rating in Firebase (2/2)

--- app/src/main/java/com/sendbird/android/sample/user/User.java
+++ app/src/main/java/com/sendbird/android/sample/user/User.java
@@ -1,14 +1,14 @@
 package com.sendbird.android.sample.user;
 
 public class User {
-    public String fname,lname,mname,birthday,gender,street,city,province,server,joined;
+    public String fname,lname,mname,birthday,gender,street,city,province,server,joined,rating_total,rooms_total;
     public int depTest;
     //public Uri filepath;
 
 
 
     public User(String fname, String lname, String mname,String birthday, String gender, String street, String city, String province,
-                int depTest, String server, String joined) {
+                int depTest, String server, String joined, String rating_total, String rooms_total) {
         this.fname = fname;
         this.lname = lname;
         this.mname = mname;
@@ -20,6 +20,8 @@ public class User {
         this.server = server;
         this.joined = joined;
         this.depTest = depTest;
+        this.rating_total = rating_total;
+        this.rooms_total = rooms_total;
     }
 
 
@@ -61,7 +63,15 @@ public class User {
         return joined;
     }
 
-    public int getDepTest() { return depTest; }
+    public int getDepTest() { return depTest;
+    }
+    public String getRating_total() {
+        return rating_total;
+    }
+
+    public String getRooms_total() {
+        return rooms_total;
+    }
 
     public int setDepTest(){ return this.depTest = depTest; }
 
